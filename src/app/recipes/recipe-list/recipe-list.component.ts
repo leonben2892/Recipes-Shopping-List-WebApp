8f92bc7af36ec0b667453510e856668ecd9a9342
Leon <leon.ben2892@gmail.com>
2019-10-13 00:19:31 +0300

    Added data & events binding to recipes and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test', 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg')
+    new Recipe('Another test recipe', 'This is simply a test', 'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
