c5f75b29075e71afe6b5d92fee31287f79eccd0e
Leon <leon.ben2892@gmail.com>
2019-10-20 13:19:27 +0300

    Added http get and put to fetch and save recipes from and into the database

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 export class HeaderComponent{
     collapsed = true;
     
+    constructor(private dataStorageService: DataStorageService){}
 
-    
     ngOnInit() {
+    }
+
+    onSaveData(){
+        this.dataStorageService.storeRecipes();
+    }
+
+    onFetchData(){
+        this.dataStorageService.fetchRecipes().subscribe();
     }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,26 +8,33 @@ import { Subject } from 'rxjs';
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
     
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A test recipe', 
-            'This is simply a test', 
-            'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'Another test recipe', 
-            'This is simply a test', 
-            'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A test recipe', 
+    //         'This is simply a test', 
+    //         'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'Another test recipe', 
+    //         'This is simply a test', 
+    //         'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/165384.jpg',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
       constructor(private slService: ShoppingListService){}
+
+      setRecipes(recipes: Recipe[]){
+          this.recipes = recipes;
+          this.recipesChanged.next(this.recipes.slice());
+      }
 
       getRecipes(){
           return this.recipes.slice();
